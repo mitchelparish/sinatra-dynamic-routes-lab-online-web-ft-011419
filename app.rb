f12b293e832ef45deb4fad82b8d162058cf6c9f6
Mitchel Parish <mitchelparish@gmail.com>
2019-02-26 21:40:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -29,7 +29,7 @@ class App < Sinatra::Base
     operation = params[:operation]
 
     if operation == "add"
-      (params[:number1].to_i + params[:number2].to_i)
+      params[:number1] + params[:number2]
 
 end
   end
