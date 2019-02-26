6cd5568d7144e6e6c85d9dccd8d7cd2cc868a6b8
Mitchel Parish <mitchelparish@gmail.com>
2019-02-26 21:30:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,31 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+
+  get '/reversename/:name' do
+    params[:name].reverse
+  end
+
+  get '/square/:number' do
+    number = params[:number].to_i
+    "#{number * number}.to_s"
+  end
+
+  get '/say/:number/:phrase' do
+
+
+
+
+  end
+
+
+
+
+
+
+
+
+
+
 
 end
